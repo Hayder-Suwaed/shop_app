3ade5c48b2199e7031d03884af5bd9403bffd42b
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-05-17 11:03:19 -0600

    Using Custom Fonts and fix bug with fontLoaded by remove node_models/.Expo/node_moels

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import { useFonts } from "@use-expo/font";
+import { StyleSheet } from "react-native";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { AppLoading } from "expo";
 
-import productsReducer from './store/reducers/products';
-import ShopNavigator from './navigation/ShopNavigator';
+import productsReducer from "./store/reducers/products";
+import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
 });
 
 const store = createStore(rootReducer);
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <ShopNavigator />
-    </Provider>
-  );
+  let [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <Provider store={store}>
+        <ShopNavigator />
+      </Provider>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
