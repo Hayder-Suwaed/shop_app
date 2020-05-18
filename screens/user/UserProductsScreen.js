3a5b86796b9875d795d76f101fe0014aa1c58293
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-05-18 10:50:32 -0600

    Reorganized ProductItem Component and added buttons to Admin page

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { FlatList , Platform} from "react-native";
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { FlatList, Button, Platform } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import HeaderButton from '../../components/UI/HeaderButton';
+import HeaderButton from "../../components/UI/HeaderButton";
 import ProductItem from "../../components/shop/ProductItem";
 import { useSelector } from "react-redux";
+import Colors from "../../constants/Colors";
 
 const UserProductsScreen = (props) => {
   const userProducts = useSelector((state) => state.products.userProducts);
@@ -17,9 +18,11 @@ const UserProductsScreen = (props) => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onViewDetails={() => {}}
-          onAddToCart={() => {}}
-        />
+          onSelect={() => {}}
+        >
+          <Button color={Colors.primary} title="Edit" onPress={() => {}} />
+          <Button color={Colors.primary} title="Delete" onPress={() => {}} />
+        </ProductItem>
       )}
     />
   );
