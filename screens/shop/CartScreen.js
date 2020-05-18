4ed8979f1372099db94421721004207294dd5678
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-05-18 06:16:30 -0600

    Added Redux Logic for Orders

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
+import React from "react";
+import { View, Text, FlatList, Button, StyleSheet } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 
-import Colors from '../../constants/Colors';
-import CartItem from '../../components/shop/CartItem';
-import * as cartActions from '../../store/actions/cart';
+import Colors from "../../constants/Colors";
+import CartItem from "../../components/shop/CartItem";
+import * as cartActions from "../../store/actions/cart";
+import * as ordesrActions from "../../store/actions/order";
 
-const CartScreen = props => {
-  const cartTotalAmount = useSelector(state => state.cart.totalAmount);
-  const cartItems = useSelector(state => {
+const CartScreen = (props) => {
+  const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const cartItems = useSelector((state) => {
     const transformedCartItems = [];
     for (const key in state.cart.items) {
       transformedCartItems.push({
@@ -16,7 +17,7 @@ const CartScreen = props => {
         productTitle: state.cart.items[key].productTitle,
         productPrice: state.cart.items[key].productPrice,
         quantity: state.cart.items[key].quantity,
-        sum: state.cart.items[key].sum
+        sum: state.cart.items[key].sum,
       });
     }
     return transformedCartItems.sort((a, b) =>
@@ -29,19 +30,22 @@ const CartScreen = props => {
     <View style={styles.screen}>
       <View style={styles.summary}>
         <Text style={styles.summaryText}>
-          Total:{' '}
+          Total:{" "}
           <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
         </Text>
         <Button
           color={Colors.accent}
           title="Order Now"
           disabled={cartItems.length === 0}
+          onPress={() => {
+            dispatch(ordesrActions.addOrder(cartItems, cartTotalAmount));
+          }}
         />
       </View>
       <FlatList
         data={cartItems}
-        keyExtractor={item => item.productId}
-        renderItem={itemData => (
+        keyExtractor={(item) => item.productId}
+        renderItem={(itemData) => (
           <CartItem
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
@@ -58,29 +62,29 @@ const CartScreen = props => {
 
 const styles = StyleSheet.create({
   screen: {
-    margin: 20
+    margin: 20,
   },
   summary: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginBottom: 20,
     padding: 10,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     elevation: 5,
     borderRadius: 10,
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
   summaryText: {
-    fontFamily: 'open-sans-bold',
-    fontSize: 18
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
   },
   amount: {
-    color: Colors.primary
-  }
+    color: Colors.primary,
+  },
 });
 
 export default CartScreen;
