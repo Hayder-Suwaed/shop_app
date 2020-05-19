4a033cd22341471d0ab99755b99571f2128441f8
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-05-19 04:40:39 -0600

    Added Basic Validation

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   View,
   ScrollView,
@@ -6,30 +6,38 @@ import {
   TextInput,
   StyleSheet,
   Platform,
-} from "react-native";
-import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { useSelector, useDispatch } from "react-redux";
+  Alert
+} from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector, useDispatch } from 'react-redux';
 
-import HeaderButton from "../../components/UI/HeaderButton";
-import * as productsActions from "../../store/actions/products";
+import HeaderButton from '../../components/UI/HeaderButton';
+import * as productsActions from '../../store/actions/products';
 
-const EditProductScreen = (props) => {
-  const prodId = props.navigation.getParam("productId");
-  const editedProduct = useSelector((state) =>
-    state.products.userProducts.find((prod) => prod.id === prodId)
+const EditProductScreen = props => {
+  const prodId = props.navigation.getParam('productId');
+  const editedProduct = useSelector(state =>
+    state.products.userProducts.find(prod => prod.id === prodId)
   );
   const dispatch = useDispatch();
 
-  const [title, setTitle] = useState(editedProduct ? editedProduct.title : "");
+  const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
+  const [titleIsValid, setTitleIsValid] = useState(false);
   const [imageUrl, setImageUrl] = useState(
-    editedProduct ? editedProduct.imageUrl : ""
+    editedProduct ? editedProduct.imageUrl : ''
   );
-  const [price, setPrice] = useState("");
+  const [price, setPrice] = useState('');
   const [description, setDescription] = useState(
-    editedProduct ? editedProduct.description : ""
+    editedProduct ? editedProduct.description : ''
   );
 
   const submitHandler = useCallback(() => {
+    if (!titleIsValid) {
+      Alert.alert('Wrong input!', 'Please check the errors in the form.', [
+        { text: 'Okay' }
+      ]);
+      return;
+    }
     if (editedProduct) {
       dispatch(
         productsActions.updateProduct(prodId, title, description, imageUrl)
@@ -40,11 +48,20 @@ const EditProductScreen = (props) => {
       );
     }
     props.navigation.goBack();
-  }, [dispatch, prodId, title, description, imageUrl, price]);
+  }, [dispatch, prodId, title, description, imageUrl, price, titleIsValid]);
 
   useEffect(() => {
     props.navigation.setParams({ submit: submitHandler });
   }, [submitHandler]);
+
+  const titleChangeHandler = text => {
+    if (text.trim().length === 0) {
+      setTitleIsValid(false);
+    } else {
+      setTitleIsValid(true);
+    }
+    setTitle(text);
+  };
 
   return (
     <ScrollView>
@@ -54,21 +71,22 @@ const EditProductScreen = (props) => {
           <TextInput
             style={styles.input}
             value={title}
-            onChangeText={(text) => setTitle(text)}
+            onChangeText={titleChangeHandler}
             keyboardType="default"
             autoCapitalize="sentences"
             autoCorrect
             returnKeyType="next"
-            onEndEditing={() => console.log("onEndEditing")}
-            onSubmitEditing={() => console.log("onSubmitEditing")}
+            onEndEditing={() => console.log('onEndEditing')}
+            onSubmitEditing={() => console.log('onSubmitEditing')}
           />
+          {!titleIsValid && <Text>Please enter a valid title!</Text>}
         </View>
         <View style={styles.formControl}>
           <Text style={styles.label}>Image URL</Text>
           <TextInput
             style={styles.input}
             value={imageUrl}
-            onChangeText={(text) => setImageUrl(text)}
+            onChangeText={text => setImageUrl(text)}
           />
         </View>
         {editedProduct ? null : (
@@ -77,7 +95,7 @@ const EditProductScreen = (props) => {
             <TextInput
               style={styles.input}
               value={price}
-              onChangeText={(text) => setPrice(text)}
+              onChangeText={text => setPrice(text)}
               keyboardType="decimal-pad"
             />
           </View>
@@ -87,7 +105,7 @@ const EditProductScreen = (props) => {
           <TextInput
             style={styles.input}
             value={description}
-            onChangeText={(text) => setDescription(text)}
+            onChangeText={text => setDescription(text)}
           />
         </View>
       </View>
@@ -95,43 +113,43 @@ const EditProductScreen = (props) => {
   );
 };
 
-EditProductScreen.navigationOptions = (navData) => {
-  const submitFn = navData.navigation.getParam("submit");
+EditProductScreen.navigationOptions = navData => {
+  const submitFn = navData.navigation.getParam('submit');
   return {
-    headerTitle: navData.navigation.getParam("productId")
-      ? "Edit Product"
-      : "Add Product",
+    headerTitle: navData.navigation.getParam('productId')
+      ? 'Edit Product'
+      : 'Add Product',
     headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Save"
           iconName={
-            Platform.OS === "android" ? "md-checkmark" : "ios-checkmark"
+            Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
           }
           onPress={submitFn}
         />
       </HeaderButtons>
-    ),
+    )
   };
 };
 
 const styles = StyleSheet.create({
   form: {
-    margin: 20,
+    margin: 20
   },
   formControl: {
-    width: "100%",
+    width: '100%'
   },
   label: {
-    fontFamily: "open-sans-bold",
-    marginVertical: 8,
+    fontFamily: 'open-sans-bold',
+    marginVertical: 8
   },
   input: {
     paddingHorizontal: 2,
     paddingVertical: 5,
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
-  },
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  }
 });
 
 export default EditProductScreen;
