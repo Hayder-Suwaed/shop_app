5dbb71ed30505902225bb0c6f590e63c342b8434
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-05-20 08:21:20 -0600

    fix bug in auth

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -26,3 +26,30 @@ export const signup = (email, password) => {
     dispatch({ type: SIGNUP });
   };
 };
+
+export const signin = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAQQTrNhhUxG34R2iIMI0lamNmb9RBzXy4',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: SIGNUP });
+  };
+};
